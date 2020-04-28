79b0e7c286def293aa6afc11befa599990abc9c4
Rams <plram2005@gmail.com>
2020-04-28 19:24:27 +0530

    adding yaml

--- run-stack.sh
+++ run-stack.sh
@@ -5,12 +5,12 @@ set -x
 STACK_NAME=$1
 ALB_LISTENER_ARN=$2
 
-# if ! aws cloudformation describe-stacks --region us-east-2 --stack-name $STACK_NAME 2>&1 > /dev/null
-# then
-#	finished_check=stack-create-complete
-# else
-#	finished_check=stack-update-complete
-# fi
+if ! aws cloudformation describe-stacks --region us-east-2 --stack-name $STACK_NAME 2>&1 > /dev/null
+then
+	finished_check=stack-create-complete
+else
+	finished_check=stack-update-complete
+fi
 
 aws cloudformation deploy \
     --region us-east-2 \
@@ -23,3 +23,5 @@ aws cloudformation deploy \
     "VPC=vpc-d28a5db9" \
     "Cluster=default" \
     "Listener=$ALB_LISTENER_ARN"
+	
+aws cloudformation wait $finished_check --region us-east-2 --stack-name $STACK_NAME 
